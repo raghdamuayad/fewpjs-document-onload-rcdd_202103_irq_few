811891b0ac37ec0f0d01aa8e41567f2787017e1e
Raghda  Muayad <therose000777@gmail.com>
2021-04-22 19:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 
  document.addEventListener("DOMContentLoaded", function() {
   //console.log("JavaScript is so cool. It lets me add text to my page programmatically");
-   alert("This is really cool!");
+   //alert("This is really cool!");
 });
+function displayResult() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+
 //document.addEventListener("DOMContentLoaded", () => {
 //   alert("This is really cool!");
 // });
