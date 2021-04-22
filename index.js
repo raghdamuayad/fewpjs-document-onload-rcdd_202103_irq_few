9a93787d74b713dd18d93d1c803aeea6b54c318a
Raghda  Muayad <therose000777@gmail.com>
2021-04-22 19:59:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// Your code goes heredocument.addEventListener("DOMContentLoaded", function() {
+
+
+ document.addEventListener("DOMContentLoaded", function() {
+
+   document.getElementById("text").innerHTML = "This is really cool!";
+});
+
+
+console.log("This is really cool!");
